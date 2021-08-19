d425accf9dbaac5cfff0a9ae1a2c7fcae08b51fc
Orlando N. Rodriguez <dfashion.corp@gmail.com>
2021-08-19 01:03:21 -0500

    Add Status bar backgroun color

--- app/src/main/java/com/ondev/rickandmorty/ui/theme/Color.kt
+++ app/src/main/java/com/ondev/rickandmorty/ui/theme/Color.kt
@@ -2,11 +2,7 @@ package com.ondev.rickandmorty.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
+val white=Color.White
 val background = Color(0xff24282f)
 val surface = Color(0xff3c3e44)
 val aLiveGreen = Color(0xff55cc44)
--- app/src/main/java/com/ondev/rickandmorty/ui/theme/Theme.kt
+++ app/src/main/java/com/ondev/rickandmorty/ui/theme/Theme.kt
@@ -6,39 +6,21 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200,
+private val RickColorPalette = darkColors(
+    primary = background,
+    primaryVariant = surface,
+    secondary = white,
     background = background,
     surface = surface
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-
 )
+
 
 @Composable
 fun RickAndMortyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable() () -> Unit,
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colors = RickColorPalette
 
     MaterialTheme(
         colors = colors,
